324d639b89df6f3a5cf522de05fe1c60a00b7452
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-05-13 12:03:57 +0800

    pseudocode

---
+++ ClassTV.py
@@ -0,0 +1,23 @@
+# Joshua Lemuel Z. Centina BS CPE 1-4
+# Class TV
+
+# Create a Python Code for creating the Class named TV and a Test Driver program named 
+# TestTV that will create two objects from Class TV and will produce the following output:
+
+# pseudocode
+# create class TV
+# constructor
+    # channel: int
+    # volumeLevel: int
+    # on: bool
+# Methods:
+    # turnon: None
+    # turnoff: None
+    # getChannel: int
+    # setChannel(channel: int): None
+    # getVolume: int
+    # setVolume(volumeLevel: int): None
+    # channelUp: None
+    # channelDown: None
+    # volumeUp: None
+    # volumeDown: None
